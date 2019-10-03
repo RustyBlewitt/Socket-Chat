3186f90856466402f208a6a0a516e24402ae5a6a
Rusty Blewitt <rusty.blewitt@griffithuni.edu.au>
2019-10-03 21:07:39 +1000

    Added authentication endpoint

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,67 +1,33 @@
-// module.exports = (app, db) => {
+module.exports = (app, db) => {
 
-
-// TODO: but should put users in db first
-
-
-//     app.post('/api/auth', function(req, res){
-//         if(!req.body) {
-//             return res.sendStatus(400);
-//         }
-//         console.log('Request received: ', req.body);
-//         const collection = db.collection('messages');
-        
-//         // Incoming body
-//         // {
-//         //     channel: String,
-//         //     message: String,
-//         //     user: String,
-//         //     date: Number, [YYYYMMDD]
-//         //     seconds: Number, 
-//         // }
-        
-//         collection.insertOne(req.body, function(err, result){
-//             if(result){
-//                 console.log('Successful insertion', result.result);
-//                 res.status(200).send(result.result);
-//             } else {
-//                 console.log('Issue: ', err);
-//                 res.status(500).send(err);
-//             }
-//         });
-        
-//     });
+    app.post('/api/auth', function(req, res){
+        if(!req.body.username && !req.body.password) {
+            return res.sendStatus(400);
+        }
+        console.log('Request received: ', req.body);
+        const collection = db.collection('users');
     
-//     }
+        // Incoming body
+        // {
+        //     username: string,
+        //     password: string,
+        // }
 
-//     // app.post('/api/auth', function (req, res) {
-//     //     if (!req.body) {
-//     //         return res.sendStatus(400);
-//     //     }
-    
-//     //     // Stored users, string to object
-//     //     var userStringJson = fs.readFileSync('./users.json', 'utf8');
-//     //     let users = JSON.parse(userStringJson);
-    
-//     //     // Attempted login becomes a user object, initially set to invalid.
-//     //     let attemptedUser = {
-//     //         username: req.body.username,
-//     //         password: req.body.password,
-//     //         valid: false
-//     //     };
-    
-//     //     // Iterate through users to check if attempted user login is valid.
-//     //     // attemptedUser is replaced with valid user if attempted user is valid.
-//     //     for (i in users) {
-//     //         if (attemptedUser.username == users[i].username && attemptedUser.password == users[i].password) {
-//     //             attemptedUser = users[i];
-//     //         }
-//     //     }
-        
-//     //     // Log whether login was successful or not
-//     //     attemptedUser.valid ? console.log('User login succesful for: ', attemptedUser)
-//     //         : console.log('User login unsuccesful for: ', attemptedUser);
-        
-//     //     // Post back attemptedUser object
-//     //     res.send(attemptedUser);
-//     // })
+
+        // Look for a corresponding user with given name and pw
+        collection.find({
+            $and: [ {'username': req.body.username}, {'password': req.body.password}]
+        }).toArray(function(err, result_array){
+            if(result_array.length > 0){
+                console.log('Login successful');
+                return res.status(200).send(result_array[0]);
+            } else if(err){
+                console.log('Internal error during authentication');
+                return res.status(500).send({"message": "Internal error during authentication"});
+            }else{
+                console.log('Invalid username or password');
+                return res.status(401).send({"message":"Invalid username or password"});
+            }
+        })
+    });
+};
